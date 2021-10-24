53a0382e28d6ec16db046081eb57db5f06be9a5e
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2021-10-24 19:55:45 +0300

    Find max consecutive ones II

---
+++ Problems/Arrays/FindMaxConsecutiveOnesII.java
@@ -0,0 +1,37 @@
+package number;
+
+/**
+ * @author Albina Gimaletdinova on 24/10/2021
+ */
+public class FindMaxConsecutiveOnesII {
+    public int findMaxConsecutiveOnes(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        if (arr.length == 1) {
+            return 1;
+        }
+
+        boolean includedOneZero = false;
+        int curLen = 0;
+        int maxLen = 0;
+        int zeroIndex = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                curLen++;
+            } else {
+                if (!includedOneZero) {
+                    curLen++;
+                    includedOneZero = true;
+                    zeroIndex = i;
+                } else {
+                    maxLen = Math.max(maxLen, curLen);
+                    curLen = 0;
+                    includedOneZero = false;
+                    i = zeroIndex;
+                }
+            }
+        }
+        return Math.max(maxLen, curLen);
+    }
+}
